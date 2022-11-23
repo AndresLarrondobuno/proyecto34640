76667d41688da64ad98918c7987843beff46eee6
Andres <andreslarrondobuno@gmail.com>
2022-11-23 09:46:57 -0300

    entregable terminado

--- appFamilia/views.py
+++ appFamilia/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
 from .models import Familiar
-from django.http import HttpResponse
+
 
 def familia(request):
-    mabel = Familiar(nombre="Mabel", edad=28, fecha_de_nacimiento=1993-12-12)
-    mabel.save()
-    cadena = "Familiar guardado: " + mabel
-    return HttpResponse(cadena)
+    
+    mabel = Familiar(nombre="Mabel", edad=28, fecha_de_nacimiento="1993-12-12")
+    alberto = Familiar(nombre="Alberto", edad=72, fecha_de_nacimiento="1950-17-3")
+    abigail = Familiar(nombre="Abigail", edad=22, fecha_de_nacimiento="1993-11-2")
+    mi_familia = {'familiares': [mabel, alberto, abigail]}
+    return render(request, 'familiares1.html', mi_familia)
